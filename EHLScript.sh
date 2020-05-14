b433e50bc29a2cf2d5d09ce91dcb3601c7c14cee
EHLcybersec <cehschoolcare@gmail.com>
2020-05-14 06:12:54 +0600

    Install all termux pkg

---
+++ EHLScript.sh
@@ -0,0 +1,14 @@
+pkg install git -y
+pkg install python -y
+pkg install python2 -y
+pkg install python3 -y
+pkg install wget
+pkg install php
+pkg install curl
+apt update -y
+apt upgrade -y
+pkg install nano
+pip2 install requests
+pip2 install mechanize
+pkg install perl
+pkg install ruby
